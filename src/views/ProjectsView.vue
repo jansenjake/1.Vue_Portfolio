0851031dd73771e565776dc7c97e20a0172ee19e
jakejansen <jansenjake599@gmail.com>
2023-01-14 10:49:43 +0200

    minor updates

--- src/views/ProjectsView.vue
+++ src/views/ProjectsView.vue
@@ -1,22 +1,27 @@
 <template>
-    <h1 class="pro">Projects</h1>
+    <h1 class="pro"> My Projects</h1>
     <!-- https://boujeebirds.netlify.app -->
     <!-- https://jansenjakeportfolio.netlify.app    p1 -->
     <!-- https://jake-calculator.netlify.app -->
 
-<div class="card" style="width: 18rem;">
+
+<div class="contain">
+  <div class="card" style="width: 18rem;">
   <img src="https://i.postimg.cc/gJVvzCXk/project1.png" class="card-img-top" alt="...">
   <div class="card-body">
-    <h5 class="card-title">My First Ever Project</h5>
+    <h5 class="card-title">My First Ever Project</h5> 
     <p class="card-text"></p>
   </div>
   <div class="card-body">
     <a href="https://jansenjakeportfolio.netlify.app" class="card-link"><button class="see">View</button></a>
   </div>
 </div>
+<h3>
+  Project Name: Jake Jansen Portfolio
+</h3>
+</div>
 
-
-
+<div class="contain">
 <div class="card" style="width: 18rem;">
   <img src="https://i.postimg.cc/SRmcJgCL/project2.png" class="card-img-top" alt="...">
   <div class="card-body">
@@ -26,9 +31,14 @@
   <div class="card-body">
     <a href="https://boujeebirds.netlify.app" class="card-link"><button class="see">View</button></a>
   </div>
+</div> 
+<h3>
+  Project Name: Boujee Birds
+</h3>
 </div>
 
 
+<div class="contain">
 <div class="card" style="width: 18rem;">
   <img src="https://i.postimg.cc/0y4Gcd1x/project3.png" class="card-img-top" alt="...">
   <div class="card-body">
@@ -39,9 +49,10 @@
     <a href="https://jake-calculator.netlify.app" class="card-link"><button class="see">View</button></a>
   </div>
 </div>
-
-
-
+<h3>
+  Project Name: Calculator
+</h3>
+</div>
 
 
 
@@ -58,7 +69,12 @@
 h1{
     margin-top: 30px;
     margin-bottom: 30px;
-}
+    font-size: 50px;
+    text-shadow: 0 0 10px blue, 0 0 20px silver;
+  }
+
+
+
 
 .pro1{
     width: 350px;
@@ -81,6 +97,7 @@ h1{
     margin-left: 15rem;
     margin-top: 30px;
     padding-top: 0;
+    margin-bottom: 25px;
 }
 
 .see{
@@ -92,6 +109,13 @@ h1{
 .pro{
     animation: project 3s;
 }
+
+.contain{
+  position: relative;
+  border: 3px solid black;
+}
+
+
 
 
 
